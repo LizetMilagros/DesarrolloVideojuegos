9c00161378469e862ebc085dfe68960a340c1964
LizetMilagros <lcuevas@unsa.edu.pe>
2024-01-07 21:56:37 -0500

    zombie quita vidas

--- Assets/Codigos/PlayerStats.cs
+++ Assets/Codigos/PlayerStats.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerStats : MonoBehaviour
 {
@@ -39,5 +40,8 @@ public class PlayerStats : MonoBehaviour
     private void Die()
     {
         Debug.Log("Moriste");
+        // Recarga el nivel actual
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
